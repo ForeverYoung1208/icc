4603715905946e8fe130d08a4c68a2a7bb615299
Igor <siafin2010@gmail.com>
2013-06-10 17:52:38 +0300

    9

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,23 +1,21 @@
 $(document).ready(function(){
-	$('#menu li').on("mouseenter", function(){
-				$(this).find('a').animate({
-				"background-color": "#FFE0E0",
-		})
-	});
-	$('#menu li').on("mouseleave", function(){
-				$(this).find('a').animate({
-				"background-color": "#FFFFFF",
-		}).queue(function(){
-	        $(this).queue("fx", [])
-        });
-	});
+	$('#menu li').hover(
+		function(){
+			$(this).find('a').filter(':not(:animated)').animate({
+				"background-color": "#FFE0E0"
+			});
+		},
+		function(){
+			$(this).find('a').animate({
+				"background-color": "#FFFFFF"
+			});
+		}
+	);
+
 	$('#menu li').on("click", function(event){
-				event.preventDefault();
-				$('#content').effect('fade');
-				$.ajax({
-					url:"services"
-				}).done(function(){
-					$('#content').effect('fade');
-				});
+		$('#content').effect('fade');
+		$('#menu').find('a').removeClass('selected',500);
+		$(this).find('a').css({'background-color':'#F2C2C2'});
+		$(this).find('a').addClass('selected',500);
 	});
 })
