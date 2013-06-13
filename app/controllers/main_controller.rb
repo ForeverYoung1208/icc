6f8228f696fd8850dc5b054a0b5a69872fec4b14
ForeverYoung1208 <siafin2010@gmail.com>
2013-06-13 23:17:47 +0300

    f

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
   def index
+  	session[:selected_page]=params[:selected_page]
+  	debugger
     respond_to do |format|
       format.html # index.html.erb
       format.js {}
