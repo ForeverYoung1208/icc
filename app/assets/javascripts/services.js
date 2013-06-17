a4262e8b12da926ec29266c2191e4a4db45f0819
ForeverYoung1208 <siafin2010@gmail.com>
2013-06-17 23:38:13 +0300

    2

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,5 +1,5 @@
 
 $(document).ready(function(){
-	area1 = new nicEditor({fullPanel : true}).panelInstance('service_text',{hasPanel : true});
+	new nicEditor({fullPanel : true}).panelInstance('service_text',{hasPanel : true});
 
 });
