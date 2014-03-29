95708c4d583c06df08c43616648365464619c0f1
ForeverYoung1208 <siafin2010@gmail.com>
2014-03-29 20:48:31 +0200

    fixed news area

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Icc
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
 
 config.active_record.default_timezone = :local
